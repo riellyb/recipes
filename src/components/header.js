132606aff6a1de1a586ea0db7458c75700cfdb49
Brendan Rielly <brielly@MBP-BRIELLY.local>
2017-09-15 14:24:34 -0600

    moved login and signup to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 export default class Header extends React.Component {
 	render() {
 		return (
 			<nav className="main-nav">
 				<h1 style={{color: '#fff'}}>Recipes</h1>
+				<div className="pull-right"><Link to='/login'><button className="btn btn-primary">Login</button></Link></div>
+				<div className="pull-right" style={{marginRight: '15px'}}><Link to='/signup'><button className="btn btn-primary">Signup</button></Link></div>
 			</nav>
 		);
 	}
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -8,8 +8,6 @@ export default class Sidebar extends React.Component {
 				<h2>Menu</h2>
 				<Link to='/'><button className="btn btn-primary">Browse Recipes</button></Link>
 				<Link to='/new-recipe'><button className="btn btn-primary">New Recipe</button></Link>
-				<Link to='/login'><button className="btn btn-primary">Login</button></Link>
-				<Link to='/signup'><button className="btn btn-primary">Signup</button></Link>
 			</aside>
 		);
 	}
