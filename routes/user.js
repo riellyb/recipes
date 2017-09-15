1fcabf9fd87a47e2ed6b6fb1cfbe75313567d678
Brendan Rielly <brendanrielly@gmail.com>
2017-09-14 21:09:30 -0600

    cleaned up user Routes

--- routes/user.js
+++ routes/user.js
@@ -9,27 +9,26 @@ module.exports = function(app) {
 
 	// Define the login route
 	router.post('/login',
-	  passport.authenticate('local-login', { successRedirect: '/',
-	                                   failureRedirect: '/',
-	                                   failureFlash: true })
+	  	passport.authenticate('local-login', { 
+		  	successRedirect: '/',
+	        failureRedirect: '/',
+	        failureFlash: true })
 	);
 
 	// process the signup form
-    router.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/', // redirect to the homepage
-        failureRedirect : '/', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
+    router.post('/signup', 
+    	passport.authenticate('local-signup', {
+	        successRedirect : '/', // redirect to the homepage
+	        failureRedirect : '/', 
+	        failureFlash : true // allow flash messages
     }));
 
-	// =====================================
-    // LOGOUT ==============================
-    // =====================================
+	// process logout
     router.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
     });
 };
-
 
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
